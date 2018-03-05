f0eaaa3825d93effd6d7afd8812b604df3ec9150
wangh0117 <wangh0117@yeah.net>
2018-03-04 20:53:00 -0600

    add the sum time function

--- LearnMkvInfo.py
+++ LearnMkvInfo.py
@@ -6,10 +6,10 @@ filenames = [x for x in os.listdir(pathname) if os.path.splitext(x)[1] == '.mkv'
 filenames.sort(key=lambda x:int(x[:2])) #列出所有视屏文件并排序
 duration_time = []
 for i in filenames:
-    print(know(pathname+'\\'+i)['duration'],i)
-
-    # duration_time =duration_time.append(know(filename+'.'+i)['duration'])
-
+    duration_time.append(know(pathname+'\\'+i)['duration'].seconds)
+# print(duration_time)
+sum_time = sum([x for x in duration_time if x <1000])
+print('total learning time is {} hour {} minutes'.format(int(sum_time/360),int((sum_time-int(sum_time/360)*360)/60)))
 # print(filenames)
 # filename = r"C:\Users\wangh\Downloads\stanford _machine_learning\1.mkv"
 # duration_time = know(filename)['duration']
