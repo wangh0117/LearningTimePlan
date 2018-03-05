28b28295c2f7fdb7b68b96f385ffb7750f5f4f9a
wangh0117 <wangh0117@yeah.net>
2018-03-04 20:57:44 -0600

    correct a mistake

--- LearnMkvInfo.py
+++ LearnMkvInfo.py
@@ -9,7 +9,7 @@ for i in filenames:
     duration_time.append(know(pathname+'\\'+i)['duration'].seconds)
 # print(duration_time)
 sum_time = sum([x for x in duration_time if x <1000])
-print('total learning time is {} hour {} minutes'.format(int(sum_time/360),int((sum_time-int(sum_time/360)*360)/60)))
+print('total learning time is {} hour {} minutes'.format(int(sum_time/3600),int((sum_time-int(sum_time/3600)*3600)/60)))
 # print(filenames)
 # filename = r"C:\Users\wangh\Downloads\stanford _machine_learning\1.mkv"
 # duration_time = know(filename)['duration']
